f88bdc0563006fe8de01881302ae293bd38090d5
Vilte N <viltenor@gmail.com>
2022-08-25 19:02:47 +0100

    all but 1 tests passed in extension

--- src/main/java/ParenthesesChecker.java
+++ src/main/java/ParenthesesChecker.java
@@ -5,21 +5,63 @@ public class ParenthesesChecker {
         }
 // split myString into substrings of 1 character
         String[] splitString = myString.split("");
-        String target1 = "(";
-        String target2 = ")";
-        int countTarget1 = 0;
-        int countTarget2 = 0;
+//        brackets ()
+        String bracket1 = "(";
+        String bracket2 = ")";
+        int countBracket1 = 0;
+        int countBracket2 = 0;
+//        braces {}
+        String brace1 = "{";
+        String brace2 = "}";
+        int countBrace1 = 0;
+        int countBrace2 = 0;
+
+//        box []
+        String box1 = "[";
+        String box2 = "]";
+        int countBox1 = 0;
+        int countBox2 = 0;
+//        angle <>
+        String angle1 = "<";
+        String angle2 = ">";
+        int countAngle1 = 0;
+        int countAngle2 = 0;
 
 // count the number of open brackets and check if it has the same number of closing brackets
 
         for (String chr : splitString) {
-            if (chr.equals(target1)) {
-                countTarget1 = countTarget1 + 1;
+            if (chr.equals(bracket1)) {
+                countBracket1 = countBracket1 + 1;
             }
-            if (chr.equals(target2)) {
-                countTarget2 = countTarget2 + 1;
+            else if (chr.equals(bracket2)) {
+                countBracket2 = countBracket2 + 1;
             }
+//            braces
+            else if (chr.equals(brace1)) {
+                countBrace1 = countBrace1 + 1;
+            }
+            else if (chr.equals(brace2)) {
+                countBrace2 = countBrace2 + 1;
+            }
+//            box
+            else if (chr.equals(box1)) {
+                countBox1 = countBox1 + 1;
+            }
+            else if (chr.equals(box2)) {
+                countBox2 = countBox2 + 1;
+            }
+//            angle
+            else if (chr.equals(angle1)) {
+                countAngle1 = countAngle1 + 1;
+            }
+            else if (chr.equals(angle2)) {
+                countAngle2 = countAngle2 + 1;
+            }
+
         } // loop ends
-        return countTarget1 == countTarget2;
+
+        return countBracket1 == countBracket2 && countBrace1==countBrace2
+        && countBox1==countBox2 && countAngle1==countAngle2;
+
     }
 }
