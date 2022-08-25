cd25d23080aa89889dac194a30178a928e6948b5
Vilte N <viltenor@gmail.com>
2022-08-25 18:35:14 +0100

    fixed comments

--- src/main/java/ParenthesesChecker.java
+++ src/main/java/ParenthesesChecker.java
@@ -3,17 +3,15 @@ public class ParenthesesChecker {
         if (myString.isEmpty()) {
             return true;
         }
-// count the number of open brackets and check if it has the same no of closing
-//         find ( and count the number of them
-//         then find ) and count
-//         compare number of ( to no of )
+// split myString into substrings of 1 character
         String[] splitString = myString.split("");
         String target1 = "(";
         String target2 = ")";
         int countTarget1 = 0;
         int countTarget2 = 0;
 
-//        check if there are any brackets at all
+// count the number of open brackets and check if it has the same number of closing brackets
+
         for (String chr : splitString) {
             if (chr.equals(target1)) {
                 countTarget1 = countTarget1 + 1;
